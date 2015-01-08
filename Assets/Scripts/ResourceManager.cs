8b0b3f52ff7d08aa5cb61f70e56879a39f8ac877
Anthony Liu <tlrnalra@gmail.com>
2015-01-08 13:17:38 -0500

    HUD skeleton

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -6,7 +6,7 @@ using System.Collections;
 namespace RTS {
 	public static class ResourceManager {
 		public static int ScrollWidth { get { return 15; } }
-		public static float ScrollSpeed { get { return 15; } }
+		public static float ScrollSpeed { get { return 5; } }
 		public static float ZoomSpeed { get { return 5; } }
 		public static float RotateAmount { get { return 10; } }
 		public static float RotateSpeed { get { return 100; } }
